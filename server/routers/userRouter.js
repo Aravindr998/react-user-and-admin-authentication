1411e2bb4bf9640c4f4059e8966ef28f1389415c
Aravind R <aravind.r@applab.qa>
2024-01-04 15:46:41 +0530

    test

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -9,5 +9,6 @@ router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
 router.get('/userdetails', authenticateUser, userController.getUserDetails)
 router.patch('/update', authenticateUser, userMiddleware.upload.single('image'), userController.saveImage)
+router.get("/test", (req, res) => res.json({key: "value"}))
 
 module.exports = router
